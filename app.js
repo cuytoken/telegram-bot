1bda18e54a777d82795f16db9fc8d3e9d587798c
Lee Marreros <lee.marreros@pucp.pe>
2022-06-11 00:40:20 -0500

    Bot sends matic and pcuy to account

--- app.js
+++ app.js
@@ -3,14 +3,38 @@ var app = require("express")();
 var bodyParser = require("body-parser");
 var axios = require("axios");
 var ethers = require("ethers");
+var {
+  DefenderRelayProvider,
+  DefenderRelaySigner,
+} = require("defender-relay-client/lib/ethers");
 app.use(bodyParser.json());
 
-var { TELEGRAM_API_KEY, SERVER_URL, WEBHOOK_DEFENDER } = process.env;
-console.log(TELEGRAM_API_KEY, SERVER_URL, WEBHOOK_DEFENDER);
+// Smart Contract connection
+var credentials = {
+  apiKey: process.env.RLYR_TLGRM_API_KEY,
+  apiSecret: process.env.RLYR_TLGRM_SECRET_KEY,
+};
+var provider = new DefenderRelayProvider(credentials);
+var signer = new DefenderRelaySigner(credentials, provider, {
+  speed: "fast",
+});
+var gasLimit = { gasLimit: 3000000 };
+var pcuyAddress = "0x914C617CB3A1C075F9116734A53FfbCF5CeD6CA9";
+var pcuyAbi = ["function test_mint(address _account, uint256 _amount)"];
+var pcuyContract = new ethers.Contract(pcuyAddress, pcuyAbi, signer);
 
+// topic from event
+var iface = new ethers.utils.Interface([
+  "event HasReceivedPcuy(address account, bool hasReceived, uint256 balance)",
+]);
+var topic = iface.getEventTopic("HasReceivedPcuy");
+
+// Telegram integratino
+var { TELEGRAM_API_KEY, SERVER_URL, WEBHOOK_DEFENDER } = process.env;
 var TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_API_KEY}`;
 var URI = `/webhook/${TELEGRAM_API_KEY}`;
 
+// Register URL in Telegram API
 async function init() {
   var res = await axios.get(
     `${TELEGRAM_API}/setWebhook?url=${SERVER_URL + URI}`
@@ -18,9 +42,9 @@ async function init() {
   console.log(res?.data);
 }
 
-async function deleteMessage(chat_id, message_id) {
+function deleteMessage(chat_id, message_id) {
   try {
-    await axios.post(`${TELEGRAM_API}/deleteMessage`, {
+    axios.post(`${TELEGRAM_API}/deleteMessage`, {
       chat_id,
       message_id,
     });
@@ -29,9 +53,9 @@ async function deleteMessage(chat_id, message_id) {
   }
 }
 
-async function sendMessage(chat_id, text) {
+function sendMessage(chat_id, text) {
   try {
-    await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    axios.post(`${TELEGRAM_API}/sendMessage`, {
       chat_id,
       text,
     });
@@ -41,7 +65,6 @@ async function sendMessage(chat_id, text) {
 }
 
 app.post(URI, async (req, res) => {
-  console.log(req.body);
   if (req && req.body && req.body.message) {
     var message = req.body.message;
     var messageId = message.message_id;
@@ -52,44 +75,58 @@ app.post(URI, async (req, res) => {
     if (!text) return res.send();
     if (!text.includes("/fund")) return res.send();
     var wallet = text.substring(text.indexOf("0x"));
-    console.log(wallet);
 
     // validate address checksum
-    await deleteMessage(chatId, messageId);
+    deleteMessage(chatId, messageId);
     try {
       wallet = ethers.utils.getAddress(wallet);
     } catch (error) {
       console.log(error.message);
-      await sendMessage(chatId, `${from}, provide a valid address!`);
+      sendMessage(chatId, `${from}, provide a valid address!`);
       return res.send();
     }
 
     // inform request to user
-    await sendMessage(
-      chatId,
-      `${from}, en breve recibirás 5325 PCUY. ¡Hasta pronto!`
+    sendMessage(chatId, `${from}, estamos procesando su pedido.`);
+
+    // call smart contract
+    var tx = await pcuyContract
+      .connect(signer)
+      .test_mint(wallet, "5325000000000000000000");
+    var response = await tx.wait(1);
+
+    var data;
+    for (var ev of response.events) {
+      if (ev.topics.includes(topic)) {
+        data = ev.data;
+      }
+    }
+
+    var [account, hasReceived, balance] = utils.defaultAbiCoder.decode(
+      ["address", "bool", "uint256"],
+      data
     );
 
-    // call defender
-    var data = JSON.stringify({
-      wallet,
-      from,
-      chatId,
-    });
+    var message = "";
+    var maticBalance = utils.parseEther("0.025");
+    var bal = (await provider.getBalance(wallet)).toString();
+    if (bal == 0) {
+      message += "Se envió 0.025 MATIC. ";
+      var tx = await signer.sendTransaction({
+        to: wallet,
+        value: maticBalance,
+      });
+      await tx.wait();
+    }
 
-    var request = {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data,
-      url: WEBHOOK_DEFENDER,
-    };
-    var response = await axios(request);
-    console.log(response.status);
+    if (hasReceived) {
+      sendMessage(chatId, `¡${from}, MATIC: ${bal}, PCUY: ${balance}!`);
+    } else {
+      message += "Se envió 5375 PCUY tokens.";
+      sendMessage(chatId, `¡${from}, ${message}!`);
+    }
+    return res && res.send && res.send();
   }
-
-  return res && res.send && res.send();
 });
 
 app.get("/", (req, res) => {
